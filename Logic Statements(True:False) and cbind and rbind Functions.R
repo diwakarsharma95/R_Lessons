f9c7f8569cc2ff44e99e5e1c703dcaa7fc4cccc9
Diwakar Sharma <diwakarsharma95@gmail.com>
2018-12-31 15:39:35 +1000

    Logic Statements(True/False) and cbind and rbind Functions

---
+++ Logic Statements(True:False) and cbind and rbind Functions.R
@@ -0,0 +1,21 @@
+#Importing the Data
+LungCapData = read.table(file.choose(),header = TRUE, sep = ",")
+
+attach(LungCapData)
+Age[1:5]
+
+temp = Age>15
+temp[1:5]
+
+temp2 = as.numeric(Age>15)
+temp2[1:5]
+
+LungCapData[1:5,]
+
+FemSmoke = Gender=="female" & Smoke=="yes"
+FemSmoke[1:5]
+
+MoreData = cbind(LungCapData,FemSmoke)
+MoreData[1:5,]
+
+rm(list=ls())
